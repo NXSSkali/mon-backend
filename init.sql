0748c1bcf2b075030d9d1a9f0bd76dc45ede33ac
NXSSkali <addinaceridion@gmail.com>
2025-11-09 10:56:42 +0100

    Add files via upload

---
+++ init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  username TEXT,
+  email TEXT UNIQUE NOT NULL,
+  password_hash TEXT NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT now()
+);
